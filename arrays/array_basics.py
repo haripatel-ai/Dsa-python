ad7aa87ebb335d19b2679592f7c9d1baa404781f
haripatel-ai <haripatel2334@gmail.com>
2026-01-17 11:06:27 +0530

    Create arrays/array_basics.py

---
+++ arrays/array_basics.py
@@ -0,0 +1,13 @@
+# DSA Day 1: Array Basics
+
+arr = [10, 20, 30, 40]
+
+print("Array elements:")
+for x in arr:
+    print(x)
+
+total = 0
+for x in arr:
+    total += x
+
+print("Sum of array:", total)
